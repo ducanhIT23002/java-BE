694c05f8bbd4511ebba09c7fb86f8725c2992a78
DucAnh <ducanh200574@gmail.com>
2025-06-24 16:34:43 +0700

    model mapper and building converter

--- src/main/java/com/ducanh/converter/BuildingSearchConverter.java
+++ src/main/java/com/ducanh/converter/BuildingSearchConverter.java
@@ -1,5 +1,51 @@
 package com.ducanh.converter;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ducanh.model.BuildingTypeDTO;
+import com.ducanh.repository.DistrictRepository;
+import com.ducanh.repository.RentAreaRepository;
+import com.ducanh.repository.entity.BuildingEntity;
+import com.ducanh.repository.entity.DistrictEntity;
+import com.ducanh.repository.entity.RentAreaEntity;
+
+@Component
 public class BuildingSearchConverter {
-     
+
+	@Autowired
+	private DistrictRepository districtRepository;
+	
+	@Autowired
+	private RentAreaRepository rentAreaRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper; 
+	
+     public BuildingTypeDTO toBuildingTypeDTO(BuildingEntity item) {
+    	 
+			BuildingTypeDTO building = modelMapper.map(item,BuildingTypeDTO.class);
+			building.setName(item.getName());
+			DistrictEntity districtEntity = districtRepository.findNameById(item.getDistrictId());
+			building.setAddress(item.getName() + ","+ item.getWard()+"," + item.getStreet() + " " + districtEntity.getName());
+			List<RentAreaEntity> rentAreaEntity = rentAreaRepository.findValueByBuildingId(item.getId());
+			
+			//java7 : ko dung stream
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < rentAreaEntity.size(); i++) {
+			    RentAreaEntity ra = rentAreaEntity.get(i);
+			    sb.append( ra.getValue() );
+			    if (i < rentAreaEntity.size() - 1) {
+			        sb.append(",");
+			    }
+			}
+			String rentvalue = sb.toString();
+			//java7 : ko dung stream
+
+			building.setRentValue(rentvalue);
+			return building;
+     }
 }
--- src/main/java/com/ducanh/service/iml/BuildingServiceIml.java
+++ src/main/java/com/ducanh/service/iml/BuildingServiceIml.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ducanh.converter.BuildingSearchConverter;
 import com.ducanh.model.BuildingTypeDTO;
 import com.ducanh.repository.BuildingRepository;
 import com.ducanh.repository.DistrictRepository;
@@ -22,35 +23,14 @@ public class BuildingServiceIml implements BuildingService{
 	private BuildingRepository buildingRepository;
 
 	@Autowired
-	private DistrictRepository districtRepository;
-	
-	@Autowired
-	private RentAreaRepository rentAreaRepository;
+	private BuildingSearchConverter buildingSearchConverter;
 	
 	@Override
 	public List<BuildingTypeDTO> findAll(Map<String, Object> param, List<String> typecode){
 		List<BuildingEntity> buildingEntities = buildingRepository.findAll(param,typecode);
 		List<BuildingTypeDTO> result = new ArrayList<BuildingTypeDTO>();
 		for (BuildingEntity item : buildingEntities) {
-			BuildingTypeDTO building = new BuildingTypeDTO();
-			building.setName(item.getName());
-			DistrictEntity districtEntity = districtRepository.findNameById(item.getDistrictId());
-			building.setAddress(item.getName() + ","+ item.getWard()+"," + item.getStreet() + " " + districtEntity.getName());
-			List<RentAreaEntity> rentAreaEntity = rentAreaRepository.findValueByBuildingId(item.getId());
-			
-			//java7 : ko dung stream
-			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < rentAreaEntity.size(); i++) {
-			    RentAreaEntity ra = rentAreaEntity.get(i);
-			    sb.append( ra.getValue() );
-			    if (i < rentAreaEntity.size() - 1) {
-			        sb.append(",");
-			    }
-			}
-			String rentvalue = sb.toString();
-			//java7 : ko dung stream
-
-			building.setRentValue(rentvalue);
+			BuildingTypeDTO building = buildingSearchConverter.toBuildingTypeDTO(item);
 			result.add(building);
 		}
 		return result;
